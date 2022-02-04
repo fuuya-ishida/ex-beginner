707ded008bc0ba985e9fc30c93a998688c3b8fa1
fuuya-ishida <fuya.ishida@rakus-partners.co.jp>
2022-02-04 12:04:46 +0900

    演習２Controller 追記(resultメソッド追加)

--- src/main/java/com/example/Exam02Controller.java
+++ src/main/java/com/example/Exam02Controller.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +12,18 @@ public class Exam02Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam02";
+	}
+	
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("result")
+	public String result(Integer num1,Integer num2) {
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("answer", num1 + num2);
+		
+		return "exam02-result";
 	}
 
 }
