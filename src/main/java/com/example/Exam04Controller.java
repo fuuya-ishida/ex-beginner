a6e21b3cb004ea08c87e23546ededf0c5512e3da
fuuya-ishida <fuya.ishida@rakus-partners.co.jp>
2022-02-04 14:51:19 +0900

    演習４Controller,Form,exam04.html,exam04-result.html 作成

---
+++ src/main/java/com/example/Exam04Controller.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.UserForm;
+
+@Controller
+@RequestMapping("exam04")
+public class Exam04Controller {
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+	
+	
+	
+	@Autowired
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
+	@RequestMapping("/result")
+	public String result(UserForm userForm,Model model) {
+		
+		
+		model.addAttribute("name",userForm.getName());
+		model.addAttribute("age",userForm.getAge());
+		model.addAttribute("comment",userForm.getComment());
+		
+		return "exam04-result";
+		
+	}
+
+}
